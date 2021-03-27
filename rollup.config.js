321740a74bffeb9b57058b8f988e634a169a62ad
yonadav <yonadav.barilan@gmail.com>
2021-03-27 05:42:50 +0300

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
