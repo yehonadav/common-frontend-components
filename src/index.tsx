930775310c4cd08ea10400ed64a4f937a65cc3ed
yonadav <yonadav.barilan@gmail.com>
2021-03-27 04:52:29 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import './styles.scss'
-
-const Package: React.FC = () => (
-  <div className="package">
-    <h2>Do cool stuff</h2>
-  </div>
-)
-
-export default Package
+export * from "./variables";
---
+++ src/variables/index.ts
@@ -0,0 +1,5 @@
+export * from './config';
+export * from './ids';
+export * from './routes';
+export * from './sizes';
+export * from './timeunit';
