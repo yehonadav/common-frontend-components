b60507bd440b728d0b3fbc1d4c965f2060a2e00c
yonadav <yonadav.barilan@gmail.com>
2021-05-12 23:57:54 +0300

    added subscribe call

--- src/services/www/service.ts
+++ src/services/www/service.ts
@@ -4,6 +4,10 @@ import {appConfig} from '../../variables';
 const call_keep_in_touch = ({email}:{email:string}):Promise<string> =>
   request.post(`${appConfig.wwwUrl}/keep_in_touch`, {email});
 
+const call_subscribe = (body:any):Promise<string> =>
+  request.post(`${appConfig.wwwUrl}/subscribe`, body);
+
 export const wwwService = {
   keep_in_touch: call_keep_in_touch,
+  subscribe: call_subscribe,
 }
