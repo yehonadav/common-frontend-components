fa305b746b73f8f9bec40510a29caaf3f87dea72
yonadav <yonadav.barilan@gmail.com>
2021-05-22 15:01:43 +0300

    fix issue where refresh returns error but api retry fails to handle the case

--- src/variables/index.ts
+++ src/variables/index.ts
@@ -1,5 +1,6 @@
 export * from './appConfig';
 export * from './ids';
+export * from "./requestStatus";
 export * from './routes';
 export * from './sizes';
 export * from './timeunit';
---
+++ src/variables/requestStatus.ts
@@ -0,0 +1,6 @@
+export enum requestStatus {
+  pending = 0,
+  loading = 1,
+  loaded = 2,
+  failed = 3,
+}
