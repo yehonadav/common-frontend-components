eef30dd4f83eda715f12b9a192fdc8b08570851e
yonadav <yonadav.barilan@gmail.com>
2021-05-24 17:46:42 +0300

    update account api endpoints

--- src/components/providers/SilentLoginProvider.tsx
+++ src/components/providers/SilentLoginProvider.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {FC, ReactNode} from "react";
 import { useAttemptSilentRefresh, useIsLogged } from '../../services'
 
-export const SilentLoginProvider:FC<{children:ReactNode, loading:ReactNode}> = ({children, loading=null}) => {
+export const SilentLoginProvider:FC<{children:ReactNode, loading?:ReactNode}> = ({children, loading=null}) => {
   useAttemptSilentRefresh();
   const isLogged = useIsLogged();
   return <>{isLogged === null ? loading : children}</>
