807b582fcff9851ee7afd07405057b84f5af4c89
OFFICE\ybarelan <ybarelan@csod.com>
2021-09-05 11:44:20 +0300

    update

--- src/components/styledComponents/Spacer.tsx
+++ src/components/styledComponents/Spacer.tsx
@@ -2,15 +2,23 @@ import {FC} from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import React from "react";
 
+interface Props {
+  size: number;
+  fullWidth?: boolean
+}
+
 export const useSpacerStyles = makeStyles({
   Spacer: {
-    margin: ({size}:{size:number})=>size/2,
+    margin: ({size}:Props)=>size/2,
+    width: ({fullWidth}:Props)=>fullWidth ? '100%' : undefined,
   },
 });
 
 export interface ISpacer extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  size?: number
+  size?: number;
+  component?: string;
+  fullWidth?: boolean
 }
 
-export const Spacer:FC<ISpacer> = ({size=10, ...props}) =>
-  <div className={useSpacerStyles({size}).Spacer} {...props}/>;
+export const Spacer:FC<ISpacer> = ({size=10, component:Component='div', fullWidth=false, ...props}) =>
+  <Component className={useSpacerStyles({size, fullWidth}).Spacer} {...props}/>;
