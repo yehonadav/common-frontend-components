4767f741e8b177baf607f0de2b5762d567d56762
OFFICE\ybarelan <ybarelan@csod.com>
2021-10-23 14:15:14 +0300

    update

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,32 +2,32 @@ import req, { AxiosRequestConfig } from 'axios'
 import { handleApiError, handleApiSuccess } from './helpers'
 import { headersJson as headers } from './headers'
 
-function get(url: string, config: AxiosRequestConfig={}):Promise<any> {
-  return req.get(url, config).then(handleApiSuccess).catch(handleApiError);
+function get<Response=any>(url: string, config: AxiosRequestConfig={}):Promise<Response> {
+  return req.get<Response>(url, config).then(handleApiSuccess).catch(handleApiError);
 }
 
-function post(url: string, body?: any, config: AxiosRequestConfig={}):Promise<any> {
-  return req.post(url, body, { ...config, headers, withCredentials: true }).then(handleApiSuccess).catch(handleApiError);
+function post<Response=any>(url: string, body?: any, config: AxiosRequestConfig={}):Promise<Response> {
+  return req.post<Response>(url, body, { ...config, headers, withCredentials: true }).then(handleApiSuccess).catch(handleApiError);
 }
 
-function put(url: string, body?: any, config: AxiosRequestConfig={}):Promise<any> {
-  return req.put(url, body, { ...config, headers }).then(handleApiSuccess).catch(handleApiError);
+function put<Response=any>(url: string, body?: any, config: AxiosRequestConfig={}):Promise<Response> {
+  return req.put<Response>(url, body, { ...config, headers }).then(handleApiSuccess).catch(handleApiError);
 }
 
-function patch(url: string, body?: any, config: AxiosRequestConfig={}):Promise<any> {
-  return req.patch(url, body, { ...config, headers }).then(handleApiSuccess).catch(handleApiError);
+function patch<Response=any>(url: string, body?: any, config: AxiosRequestConfig={}):Promise<Response> {
+  return req.patch<Response>(url, body, { ...config, headers }).then(handleApiSuccess).catch(handleApiError);
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
-function _delete(url: string, config: AxiosRequestConfig={}):Promise<any> {
-  return req.delete(url, config).then(handleApiSuccess).catch(handleApiError);
+function _delete<Response=any>(url: string, config: AxiosRequestConfig={}):Promise<Response> {
+  return req.delete<Response>(url, config).then(handleApiSuccess).catch(handleApiError);
 }
 
-function putFile(url: string, key: string, file: any, config: AxiosRequestConfig={}):Promise<any> {
+function putFile<Response=any>(url: string, key: string, file: any, config: AxiosRequestConfig={}):Promise<Response> {
   const data = new FormData();
   data.append(key, file);
 
-  return req.put(url, data, config)
+  return req.put<Response>(url, data, config)
     .then(handleApiSuccess)
     .catch(handleApiError);
 }
--- src/api/requestAuth.ts
+++ src/api/requestAuth.ts
@@ -3,37 +3,37 @@ import { authCall } from "./auth";
 import { handleApiSuccess, handleApiError } from "./helpers";
 import { headersJson, authHeader } from './headers'
 
-function get(url: string, config: AxiosRequestConfig={}):Promise<any> {
+function get<Response=any>(url: string, config: AxiosRequestConfig={}):Promise<Response> {
   return authCall(() => req.get(url, {...config, headers: authHeader()}))()
     .then(handleApiSuccess).catch(handleApiError);
 }
 
-function post(url: string, body?: any, config: AxiosRequestConfig={}):Promise<any> {
+function post<Response=any>(url: string, body?: any, config: AxiosRequestConfig={}):Promise<Response> {
   return authCall(() => req.post(url, body, {...config,
     headers: { ...headersJson, ...authHeader() },
     withCredentials: true,
   }))().then(handleApiSuccess).catch(handleApiError);
 }
 
-function put(url: string, body?: any, config: AxiosRequestConfig={}):Promise<any> {
+function put<Response=any>(url: string, body?: any, config: AxiosRequestConfig={}):Promise<Response> {
   return authCall(() => req.put(url, body, {...config,
     headers: { ...headersJson, ...authHeader() },
   }))().then(handleApiSuccess).catch(handleApiError);
 }
 
-function patch(url: string, body?: any, config: AxiosRequestConfig={}):Promise<any> {
+function patch<Response=any>(url: string, body?: any, config: AxiosRequestConfig={}):Promise<Response> {
   return authCall(() => req.patch(url, body, {...config,
     headers: { ...headersJson, ...authHeader() },
   }))().then(handleApiSuccess).catch(handleApiError);
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
-function _delete(url: string, config: AxiosRequestConfig={}):Promise<any> {
+function _delete<Response=any>(url: string, config: AxiosRequestConfig={}):Promise<Response> {
   return authCall(() => req.delete(url, {...config, headers: authHeader()}))()
     .then(handleApiSuccess).catch(handleApiError);
 }
 
-function putFile(url: string, key: string, file: any, config: AxiosRequestConfig={}):Promise<any> {
+function putFile<Response=any>(url: string, key: string, file: any, config: AxiosRequestConfig={}):Promise<Response> {
   const data = new FormData();
   data.append(key, file);
 
