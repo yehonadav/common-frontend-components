4069374df3b39673ff47235cbcb97934abc8bc33
yonadav <yonadav.barilan@gmail.com>
2021-11-03 17:09:54 +0200

    update

--- src/utils/clock.ts
+++ src/utils/clock.ts
@@ -133,3 +133,19 @@ export const datetimePretty:DatePrettyWrappedFunction = datePrettyWrapper((date:
 export const datePretty:DatePrettyWrappedFunction = datePrettyWrapper((date:Date):string => {
   return date.toLocaleDateString();
 });
+
+const intervals = [
+  { label: 'year', seconds: 31536000 },
+  { label: 'month', seconds: 2592000 },
+  { label: 'day', seconds: 86400 },
+  { label: 'hour', seconds: 3600 },
+  { label: 'minute', seconds: 60 },
+  { label: 'second', seconds: 1 }
+];
+
+export function timeSince(date:Date) {
+  const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+  const interval = intervals.find(i => i.seconds < seconds) || intervals[0];
+  const count = Math.floor(seconds / interval.seconds);
+  return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`;
+}
