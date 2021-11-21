2d2909124acddf947d429d46fa093d30737f265f
yonadav <yonadav.barilan@gmail.com>
2021-11-22 00:01:48 +0200

    update

--- src/components/navbars/appbar/Appbar.tsx
+++ src/components/navbars/appbar/Appbar.tsx
@@ -9,8 +9,9 @@ import {useColor} from "./useColor";
 import { bodyScroll } from '../../../services/scroll/Scroll';
 import { PropTypes } from '@material-ui/core'
 import React from 'react';
+import { AppBarProps } from '@material-ui/core/AppBar/AppBar'
 
-export const Appbar:FC<{component:FC<{color:PropTypes.Color}>}> = ({component:Component}) => {
+export const Appbar:FC<{component:FC<{color:PropTypes.Color}>;appBarProps:AppBarProps}> = ({component:Component, appBarProps={}}) => {
   const classes = useAppbarStyles();
   const isScrollingUp = bodyScroll.useIsScrollingUp();
   const isScrolledToTop = bodyScroll.useIsScrolledToTop();
@@ -20,7 +21,7 @@ export const Appbar:FC<{component:FC<{color:PropTypes.Color}>}> = ({component:Co
   return (
     <>
       <HideOnScroll scroller={bodyScroll}>
-        <AppBar className={classes.appbar}>
+        <AppBar className={classes.appbar} {...appBarProps}>
           <Toolbar className={toolBarClass}>
             <Component color={color}/>
           </Toolbar>
